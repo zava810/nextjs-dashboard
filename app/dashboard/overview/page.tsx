89db542d4a1cfcf001991b07335c4c293a4e2e7d
viml kumar <vimalkg@gmail.com>
2024-11-19 07:14:14 +0530

    pgdb chapter

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -2,10 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { hin115_font, lusitana } from '@/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
- 
+// import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'; // remove fetchRevenue
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+
 export default async function Page() {
-    const revenue = await fetchRevenue();
+    //const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
     const {
         numberOfInvoices,
@@ -29,7 +32,10 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        { <RevenueChart revenue={revenue} /> }
+        {/* <RevenueChart revenue={revenue} /> */}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         { <LatestInvoices latestInvoices={latestInvoices} /> }
       </div>
     </main>
