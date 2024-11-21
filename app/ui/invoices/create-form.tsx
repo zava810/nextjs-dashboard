3853368c396dc594952271ce15c36446b65e1980
viml kumar <vimalkg@gmail.com>
2024-11-22 02:02:58 +0530

    zava8.github.io fb.com/ztrabc

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -42,7 +42,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         {/* Invoice Amount */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Choose an amount
+            choose an amount
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
@@ -51,7 +51,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 name="amount"
                 type="number"
                 step="0.01"
-                placeholder="Enter USD amount"
+                placeholder="enter usdollar amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
